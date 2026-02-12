7a75be79aa51fb450e247b999ed5a4e2db110fab
mika <smileharpy@gmail.com>
2026-02-12 14:02:32 +0530

    Add files via upload

---
+++ p2.py
@@ -0,0 +1,11 @@
+game=[[0, 0, 0,
+       0, 0, 0,
+       0, 0, 0,]]
+#print("0 1 2")
+#count = 0
+#for row in game:
+ #   print(count, row)
+#count =+1
+print("a b c")
+for count, row in enumerate(game):
+        print(count, row)
