1db1c1b0272303f99735a02c0902763d23599a55
mika <smileharpy@gmail.com>
2026-02-12 14:45:38 +0530

    Update p2.py

--- p2.py
+++ p2.py
@@ -1,6 +1,7 @@
 game=[[0, 0, 0,
        0, 0, 0,
        0, 0, 0,]]
+game[0][1]=0
 #print("0 1 2")
 #count = 0
 #for row in game:
@@ -9,3 +10,5 @@ game=[[0, 0, 0,
 print("a b c")
 for count, row in enumerate(game):
         print(count, row)
+l=[1,2,3,4,5]
+print(l[0:3])
